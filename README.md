d924fbdbda9641d46b544ae03c59a1213083bc83
seveibar <seveibar@gmail.com>
2025-08-07 21:52:35 -0700

    wip

---
+++ README.md
@@ -0,0 +1,12 @@
+# RP2040 Zero
+
+A tiny development board for the RP2040 microcontroller featuring...
+
+- A USB-C Port
+- 5V to 3.3V Converter using RT9013-33 LDO Regulator
+- A configurable multicolor LED (WS2812B)
+- Flash Memory (W25Q16JVUXIQ)
+
+Based on the [Waveshare RP2040-Zero](https://www.waveshare.com/wiki/RP2040-Zero) board.
+
+- https://www.waveshare.com/wiki/RP2040-Zero [(schematic)](https://files.waveshare.com/upload/4/4c/RP2040_Zero.pdf)
---
+++ lib/VoltageRegulator.tsx
@@ -0,0 +1,45 @@
+import { RT9013_33GB } from "../imports/RT9013_33GB";
+
+export const VoltageRegulator = () => (
+	<group>
+		<capacitor
+			name="C6"
+			schOrientation="vertical"
+			footprint="0402"
+			capacitance="2.2uF"
+			schX={-4}
+		/>
+		<capacitor
+			name="C1"
+			schOrientation="vertical"
+			footprint="0402"
+			capacitance="2.2uF"
+			schX={-3.2}
+		/>
+		<capacitor
+			name="C2"
+			schOrientation="vertical"
+			footprint="0402"
+			capacitance="2.2uF"
+			schX={-2.4}
+		/>
+		<capacitor
+			name="C5"
+			schOrientation="vertical"
+			footprint="0402"
+			capacitance="1uF"
+			schX={2}
+		/>
+		<RT9013_33GB
+			name="U1"
+			connections={{
+				VIN: ["C6.1", "C1.1", "C2.1", "net.VSYS"],
+				GND: ["C6.2", "C1.2", "C2.2", "net.GND"],
+				EN: "U1.1",
+				VOUT: ["net.V3_3", "C5.1"],
+			}}
+		/>
+		<trace from="C4.2" to="net.GND" />
+		<trace from="C5.2" to="net.GND" />
+	</group>
+);
