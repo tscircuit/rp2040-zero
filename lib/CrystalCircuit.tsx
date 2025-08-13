397efed98ee5c9b6b586d376f8f3b458af9be16e
seveibar <seveibar@gmail.com>
2025-08-12 20:48:14 -0700

    finish crystal circuit

--- lib/CrystalCircuit.tsx
+++ lib/CrystalCircuit.tsx
@@ -2,27 +2,38 @@ import { ABM8_272_T3 } from "../imports/ABM8_272_T3"
 
 export const CrystalCircuit = () => (
   <group>
-    <resistor name="R8" resistance="1k" schOrientation="vertical" />
-    <capacitor schOrientation="vertical" name="C17" capacitance="15pF" />
-    <capacitor schOrientation="vertical" name="C16" capacitance="15pF" />
+    <resistor
+      name="R8"
+      resistance="1k"
+      schOrientation="vertical"
+      connections={{
+        pin1: "net.XOUT",
+      }}
+    />
+    <capacitor
+      schOrientation="vertical"
+      name="C17"
+      capacitance="15pF"
+      connections={{
+        pin2: "net.GND",
+      }}
+    />
+    <capacitor
+      schOrientation="vertical"
+      name="C16"
+      capacitance="15pF"
+      connections={{
+        pin2: "net.GND",
+      }}
+    />
     <ABM8_272_T3
       name="X1"
       connections={{
-        pin1: "C16.1",
+        pin1: ["C16.1", "net.XIN"],
         pin2: "net.GND",
         pin3: "net.GND",
-        pin4: "C17.1",
+        pin4: ["C17.1", "R8.2"],
       }}
     />
-    {/* <crystal
-      name="X1"
-      frequency="12MHz"
-      loadCapacitance="10pF"
-      pinVariant="four_pin"
-      connections={{
-      
-      }}
-      // manufacturerPartNumber="ABM8-272-T3"
-    /> */}
   </group>
 )
